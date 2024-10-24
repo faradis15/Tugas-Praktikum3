9c19b63811d3293f1a88f0ac36a6157ce0629d9c
faradis15 <2300016113@webmail.uad.ac.id>
2024-10-24 20:50:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const addButton = document.getElementById('addButton');
 const taskList = document.getElementById('taskList');
 const taskInput = document.getElementById('newTask');
+const ubahWarnaBtn = document.getElementById('ubahWarna');
+const ubahUkuranFont = document.getElementById('ubahUkuranFont');
+const ubahModeBtn = document.getElementById('ubahMode');
+const ubahGayaFontBtn = document.getElementById('ubahGayaFont');
+const paragraf = document.querySelector('p');
 
 addButton.addEventListener('click', addTask);
 
 function addTask() {
-    const taskInputValue = taskInput.value.trim(); // Ambil nilai dari input dan hapus spasi di awal/akhir
+    const taskInputValue = taskInput.value.trim();
 
     if (taskInputValue === '') {
         alert('Please enter a task.');
         return;
     }
 
-    // Membuat elemen baris tabel (tr) dan kolom (td)
     const tr = document.createElement('tr');
     tr.innerHTML = `
         <td>${taskInputValue}</td>
@@ -40,3 +44,29 @@ function editTask(element) {
         taskTd.textContent = newTask;
     }
 }
+let isDarkMode = false;
+let fontStyle = 'serif';
+const warna = ['lightblue', 'orange', 'chocolate', 'grey', 'pink'];
+
+ubahWarnaBtn.addEventListener('click', () => {
+  const warnaAcak = warna[Math.floor(Math.random() * warna.length)];
+  document.body.style.backgroundColor = warnaAcak;
+});
+
+ubahUkuranFont.addEventListener('input', () => {
+  document.body.style.fontSize = ubahUkuranFont.value + 'px';
+});
+
+ubahModeBtn.addEventListener('click', () => {
+  isDarkMode = !isDarkMode;
+  document.body.style.backgroundColor = isDarkMode ? 'black' : 'white';
+  document.body.style.color = isDarkMode ? 'white' : 'black';
+
+  judul.style.color = isDarkMode ? 'white' : 'black';
+  paragraf.style.color = isDarkMode ? 'white' : 'black';
+});
+
+ubahGayaFontBtn.addEventListener('click', () => {
+  fontStyle = fontStyle === 'serif' ? 'sans-serif' : 'serif';
+  document.body.style.fontFamily = fontStyle;
+});
